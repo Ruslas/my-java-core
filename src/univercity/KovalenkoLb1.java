8051a9c66cf38b65aca22b6d99b8048feb100154
Ruslas <ma.97rn@gmail.com>
2018-10-06 16:10:09 +0300

    Try to comm

--- src/univercity/KovalenkoLb1.java
+++ src/univercity/KovalenkoLb1.java
@@ -3,7 +3,5 @@ package univercity;
 public class KovalenkoLb1 {
     public static void main(String[] args) {
         System.out.println("DDDD");
-        System.out.println("fwfefwefwefwefwefwefwefwefwe");
-        System.out.println("fwfefwefwefwefwefwefwefwefwe"); System.out.println("fwfefwefwefwefwefwefwefwefwe");
     }
 }
