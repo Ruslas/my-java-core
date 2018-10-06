8a9cda0ab93835004a9bc4e0e9cc938e9e20d902
Ruslas <ma.97rn@gmail.com>
2018-10-06 22:43:51 +0300

    Lesson 5 faktorial

--- src/lesson5/homeWork/Faktorial.java
+++ src/lesson5/homeWork/Faktorial.java
@@ -5,15 +5,19 @@ import java.util.Random;
 public class Faktorial {
     public static void main(String[] args) {
         final Random random = new Random();
-        long n = random.nextInt(50);
+        long n = random.nextInt(20);
         System.out.println("n = " + n);
         long fn = factorial(n);
         System.out.println("n! = " + fn);
     }
 
     public static long factorial(long n) {
-        for (long i = n - 1; i > 1; i--) {
-            n *= i;
+        if (n != 0) {
+            for (long i = n - 1; i > 1; i--) {
+                n *= i;
+            }
+        } else {
+            n = 1;
         }
         return n;
     }
