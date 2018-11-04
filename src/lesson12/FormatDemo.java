713723704f570a52423f1e62c844409f1f69ad67
student1 <musicsupermax@gmail.com>
2018-11-04 11:32:48 +0200

    Lesson 12 class work StringBuilder

---
+++ src/lesson12/FormatDemo.java
@@ -0,0 +1,20 @@
+package lesson12;
+
+import java.util.Formatter;
+
+/**
+ * Created by student on 04.11.2018.
+ */
+public class FormatDemo {
+    public static void main(String[] args) {
+        print("Ivanov", 5, "Math");
+
+    }
+
+    public static void print(String sername, int mark, String subject) {
+        Formatter formatter = new Formatter();
+        formatter.format("Студент %s получил %d по %s", sername, mark, subject);
+        System.out.println(formatter);
+    }
+
+}
