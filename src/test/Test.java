fddae8f735a5b8c4809ec2123a0441d432da297d
Ruslas <ma.97rn@gmail.com>
2018-12-01 22:52:15 +0200

    Online store try/catch. Lesson 18 homework

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,19 +2,6 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        A c = new C(1, 2, 5, 6);
 
-        System.out.println(c.a + " " + c.b + " " + c.c + " " + c.d);
-
-        C c1 = (C) c;
-
-        System.out.println(c1.a + " " + c1.b + " " + c1.c + " " + c1.d + " " + c1.e + " " + c1.n);
-        System.out.println(c.getA() + " " + c.getB() + " " + c.getC());
-
-        B b1 = (B) c;
-
-        System.out.println(b1.a + " " + b1.b + " " + b1.c + " " + b1.d + " " + b1.e);
-
-        System.out.println(((C) c).getSuperA() + " " + ((C) c).getSuperB());
     }
 }
